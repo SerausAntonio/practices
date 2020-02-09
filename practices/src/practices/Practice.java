56936c174aadb9ad99a2309e4c53db4cd99dfbeb
Antonio Seraus <antonio.seraus@gmail.com>
2020-02-09 19:31:11 +0100

    first commit javaproject

---
+++ practices/src/practices/Practice.java
@@ -0,0 +1,16 @@
+package practices;
+
+public class Practice {
+
+	public Practice(){
+		 System.out.println("Inside practice");
+		 
+	}
+	public static void main(String[] args) {
+		System.out.println("Hallo world");
+
+		Practice App = new Practice();
+		
+	}
+
+}
